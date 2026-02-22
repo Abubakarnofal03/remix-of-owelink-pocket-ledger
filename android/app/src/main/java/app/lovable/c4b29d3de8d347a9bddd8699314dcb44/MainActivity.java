ef318d6545927c4b49d89bbf7d524d906781e96d
AbubakarNofal03 <abubakarnofal0786@gmail.com>
2026-02-23 03:23:33 +0500

    updated widgets

--- android/app/src/main/java/app/lovable/c4b29d3de8d347a9bddd8699314dcb44/MainActivity.java
+++ android/app/src/main/java/app/lovable/c4b29d3de8d347a9bddd8699314dcb44/MainActivity.java
@@ -2,4 +2,11 @@ package app.lovable.c4b29d3de8d347a9bddd8699314dcb44;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+import android.os.Bundle;
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(WidgetBridge.class);
+        super.onCreate(savedInstanceState);
+    }
+}
